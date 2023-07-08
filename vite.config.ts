9093fb73459faa5da11c4204b68508a6586adbe3
JonLuca DeCaro <jonluca.decaro@gmail.com>
2023-07-08 16:11:12 -0700

    feat: add dist and src files to output, bump versions

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,20 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    env: process.env,
     environment: "node",
     dir: "test",
     watch: false,
-    testTimeout: 5000,
     passWithNoTests: true,
     reporters: ["verbose"],
-    coverage: { reporter: ["lcov", "html", "text"] },
+    coverage: {
+      reporter: ["json", "text", "html", "lcov"],
+      provider: "v8",
+    },
     deps: {
       interopDefault: true,
       registerNodeLoader: true,
     },
+    testTimeout: 60 * 1000 * 60,
   },
 });
